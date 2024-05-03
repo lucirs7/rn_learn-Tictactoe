8280b58458df28d7a857081dc5885c6f708b0f5a
lucirs7 <luciars787@gmail.com>
2024-05-03 14:13:00 +0200

    Finished logic for all functions.

--- App.tsx
+++ App.tsx
@@ -15,23 +15,59 @@ export default function App() {
       setIsCross(!items.at(itemIndex));
     }
 
-    //TODO Check for win
+    winGame();
   };
 
-  const chooseItemIcon = () => {
-    //TODO Choose appropriate icon
+  const chooseItemIcon = (itemIndex: number) => {
+    if (items.at(itemIndex) !== 'empty') {
+      return items[itemIndex] ? 'cross' : 'circle';
+    } else {
+      return 'pencil';
+    }
   };
 
-  const chooseItemColour = () => {
-    //TODO Chose colour for icon
+  const chooseItemColour = (itemIndex: number) => {
+    if (items.at(itemIndex) !== 'empty') {
+      return items[itemIndex] ? 'orange' : 'blue';
+    } else {
+      return 'black';
+    }
   };
 
   const resetGame = () => {
-    //TODO Reset all values and states
+    items = new Array(9).fill('empty');
+    setWinMessage('');
   };
 
   const winGame = () => {
-    //TODO Decide/Check winner
+    let winMsg: string = '';
+
+    if ((items.at(0) !== 'empty') && (items.at(0) === items.at(1)) && items.at(1) === items.at(2)) {
+      winMsg = (items.at(0) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(3) !== 'empty') && (items.at(3) === items.at(4)) && items.at(4) === items.at(5)) {
+      winMsg = (items.at(3) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(6) !== 'empty') && (items.at(6) === items.at(7)) && items.at(7) === items.at(8)) {
+      winMsg = (items.at(6) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(0) !== 'empty') && (items.at(0) === items.at(3)) && items.at(3) === items.at(6)) {
+      winMsg = (items.at(0) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(1) !== 'empty') && (items.at(1) === items.at(4)) && items.at(4) === items.at(7)) {
+      winMsg = (items.at(1) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(2) !== 'empty') && (items.at(2) === items.at(5)) && items.at(5) === items.at(8)) {
+      winMsg = (items.at(2) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(0) !== 'empty') && (items.at(0) === items.at(4)) && items.at(4) === items.at(8)) {
+      winMsg = (items.at(0) ? 'cross' : 'circle') + ' win!';
+
+    } else if ((items.at(2) !== 'empty') && (items.at(2) === items.at(4)) && items.at(4) === items.at(6)) {
+      winMsg = (items.at(2) ? 'cross' : 'circle') + ' win!';
+    }
+
+    setWinMessage(winMsg);
   };
 
   return (
